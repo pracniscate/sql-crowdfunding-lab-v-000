73e8044abeee89af10e3e48b0d32095c13e3e31a
Lorena <lorena.giddens@outlook.com>
2018-07-16 19:52:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,9 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, projects.funding_goal - SUM(pledges.amount)
  FROM projects
  INNER JOIN pledges
- ON projects.id = pledges.project_id"
+ ON projects.id = pledges.project_id
+ GROUP BY projects.title
+ HAVING projects.funding_goal - SUM(pledges.amount)"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
